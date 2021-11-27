a44cd7251d4f28d15919ce311fb64bbec36dd860
RohanGautam <rohan1gautam@gmail.com>
2021-11-27 16:04:15 +0800

    update dh script and add nfs processes

--- mitmattack/basics/diffiehellman.py
+++ mitmattack/basics/diffiehellman.py
@@ -1,9 +1,14 @@
 # Variables Used
-sharedPrime = 23    # p
-sharedBase = 5      # g
+sharedPrime = 191907783019725260605646959711    # p
+sharedBase = 2      # g
 
-aliceSecret = 6     # a
-bobSecret = 15      # b
+aliceSecret = 539727294718671     # a
+bobSecret = 499314731500955      # b
+# sharedPrime = 23    # p
+# sharedBase = 5      # g
+
+# aliceSecret = 6     # a
+# bobSecret = 15      # b
 
 # Begin
 print("Publicly Shared Variables:")
@@ -11,19 +16,19 @@ print("    Publicly Shared Prime: ", sharedPrime)
 print("    Publicly Shared Base:  ", sharedBase)
 
 # Alice Sends Bob A = g^a mod p
-A = (sharedBase**aliceSecret) % sharedPrime
+A = pow(sharedBase, aliceSecret, sharedPrime)
 print("\n  Alice Sends Over Public Chanel: ", A)
 
 # Bob Sends Alice B = g^b mod p
-B = (sharedBase ** bobSecret) % sharedPrime
+B = pow(sharedBase, bobSecret, sharedPrime)
 print("Bob Sends Over Public Chanel: ", B)
 
 print("\n------------\n")
 print("Privately Calculated Shared Secret:")
 # Alice Computes Shared Secret: s = B^a mod p
-aliceSharedSecret = (B ** aliceSecret) % sharedPrime
+aliceSharedSecret = pow(B, aliceSecret, sharedPrime)
 print("    Alice Shared Secret: ", aliceSharedSecret)
 
 # Bob Computes Shared Secret: s = A^b mod p
-bobSharedSecret = (A**bobSecret) % sharedPrime
+bobSharedSecret = pow(A, bobSecret, sharedPrime)
 print("    Bob Shared Secret: ", bobSharedSecret)
--- mitmattack/driver.py
+++ mitmattack/driver.py
@@ -1,7 +1,9 @@
+import subprocess
 from client import Client
 from server import Server
 from mitm import Mitm
 from utils.log_config import logging
+import subprocess
 
 log = logging.getLogger(__name__)
 
@@ -22,10 +24,28 @@ if __name__ == '__main__':
     )
     p, g, B = serverhello['p'], serverhello['g'], serverhello['B']
     client.set_server_public(B)
+    A = client.send_public(p, g)
     server.set_client_public(
         mitm.send_client_public(
-            client.send_public(p, g)
+            A
         )
     )
     server.compute_shared_secret()
     client.compute_shared_secret()
+    # NFS commands
+    # Get the largest primes:
+    res = subprocess.run(
+        f'python ./cado-nfs/cado-nfs.py {p} -t all'.split(), capture_output=True)
+    # largest prime factor of p-1
+    print(res.stdout.decode('utf-8'))
+    # largest_prime_factor = max([int(x)
+    #                            for x in res.stdout.decode('utf-8').split()])
+    # log.info(f"largest prime factor of p-1: {largest_prime_factor}")
+    # log.info('Calculating client secret')
+    # res = subprocess.run(
+    #     f'python ./cado-nfs/cado-nfs.py -dlp -ell {largest_prime_factor} target={A} {p}'.split(), capture_output=True)
+    # computed_client_secret = int((res.stdout.decode('utf-8').strip()))
+    # log.info(f"DLP soln: {computed_client_secret}")
+    # if computed_client_secret != client.secret:
+    #     log.error(
+    #         f"DOES NOT MATCH: {computed_client_secret} != {client.secret}")
