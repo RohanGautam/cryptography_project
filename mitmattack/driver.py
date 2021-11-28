cd4ecd176f950ef16c613e46e8e0057c3fdd1646
RohanGautam <rohan1gautam@gmail.com>
2021-11-28 12:06:33 +0800

    add comments

--- mitmattack/driver.py
+++ mitmattack/driver.py
@@ -15,32 +15,35 @@ if __name__ == '__main__':
     mitm = Mitm()
     client = Client()
 
-    # client hello
+    # client hello to server
     server.negotiate_cipher_suite(
         mitm.forward_client_hello(
             client.hello()
         )
     )
+
+    # server hello to client, with server public params
     client.get_server_hello(
         mitm.forward_server_hello(
             server.hello()
         )
     )
-    # p, g, B = serverhello['p'], serverhello['g'], serverhello['B']
-    # client.set_server_public(B)
 
+    # exchange public parameters of client
     server.set_client_public(
         mitm.send_client_public(
             client.send_public()
         )
     )
+    # server and client compute shared secret
     server.compute_shared_secret()
     client.compute_shared_secret()
-    # the special part
+    # the mitm server computes the shared secret by solving the DLP
     mitm.compute_shared_secret()
 
-    S = client.shared_secret
-    server.init_aes(client.iv)  # publicly known, so share freely
+    # share client AES IV with server (public)
+    server.init_aes(client.iv)
+    # An example of AES interception by the MITM server.
     server.parse_client_msg(
         mitm.forward_client_to_server(
             client.aes_encrypt("a secret message".encode()),
